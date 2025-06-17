fc0c199f46762f01a7d5620049b5c01081771825
Bilal Khan <bilalkhan.devse@gmail.com>
2025-06-17 09:38:18 +0500

    pushing to dev

---
+++ src/main/java/com/yoursocial/YourSocialApplication.java
@@ -0,0 +1,13 @@
+package com.yoursocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoursocial/YourSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoursocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
