da0a49e4e0cc75c2cc974cb1cf73829157c5516f
Bilal Khan <bilalkhan.devse@gmail.com>
2025-07-30 02:11:53 +0500

    Update WebSocketConfigurer.java

--- src/main/java/com/yoursocial/config/websocket/WebSocketConfigurer.java
+++ src/main/java/com/yoursocial/config/websocket/WebSocketConfigurer.java
@@ -6,30 +6,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/web-socket")
-//                .setAllowedOriginPatterns("*")
-//                        .withSockJS();
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setApplicationDestinationPrefixes("/app");
-//        registry.enableSimpleBroker("/group", "/user", "/chat");
-//        registry.setUserDestinationPrefix("/user");
-//    }
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setApplicationDestinationPrefixes("/app");
-//        registry.enableSimpleBroker("/topic", "/queue");
-//        registry.setUserDestinationPrefix("/user");
-//    }
-//}
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
